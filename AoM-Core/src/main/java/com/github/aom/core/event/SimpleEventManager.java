3e8f505a27f675cfbddd653d2eb3aa31aaad39b6
Agustin <agustin@siegenthaler.me>
2015-06-02 20:21:47 -0300

    Fixed old license.

--- AoM-Core/src/main/java/com/github/aom/core/event/SimpleEventManager.java
+++ AoM-Core/src/main/java/com/github/aom/core/event/SimpleEventManager.java
@@ -1,13 +1,13 @@
-/*
- * This file is part of Ghrum, licensed under the Apache 2.0 License.
+/**
+ * This file is part of -AoM--Server, licensed under the APACHE License.
  *
- * Copyright (c) 2014-2015 Agustin Alvarez <wolftein1@gmail.com>
+ * Copyright (c) 2015 AoW Powered <https://github.com/AO-Modding>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
