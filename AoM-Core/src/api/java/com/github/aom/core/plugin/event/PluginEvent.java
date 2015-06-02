f64725c98c66b047b28e407a3341997091ba10e7
Agustin <agustin@siegenthaler.me>
2015-06-02 20:53:55 -0300

    Added basic plug-in events: PluginEnabledEvent and PluginDisabledEvent.

---
+++ AoM-Core/src/api/java/com/github/aom/core/plugin/event/PluginEvent.java
@@ -0,0 +1,48 @@
+/**
+ * This file is part of -AoM--Server, licensed under the APACHE License.
+ *
+ * Copyright (c) 2015 AoW Powered <https://github.com/AO-Modding>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aom.core.plugin.event;
+
+import com.github.aom.core.event.Event;
+import com.github.aom.core.plugin.Plugin;
+
+/**
+ * An {@link Event} used to signify a {@link Plugin} based event.
+ */
+public abstract class PluginEvent extends Event {
+    private final Plugin mPlugin;
+
+    /**
+     * Default constructor for {@link PluginEvent}.
+     *
+     * @param plugin        The plug-in of the event.
+     * @param isCancellable True if the event is cancellable.
+     */
+    public PluginEvent(Plugin plugin, boolean isCancellable) {
+        super(isCancellable);
+        mPlugin = plugin;
+    }
+
+    /**
+     * Retrieves the {@link Plugin} of the event.
+     *
+     * @return The plug-in of the event.
+     */
+    public final Plugin getPlugin() {
+        return mPlugin;
+    }
+}
